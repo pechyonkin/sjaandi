138a98bf61188822c5c954f5cc4ab9b54556e701
pechyonkin <maxim.pechyonkin@gmail.com>
2019-07-09 13:21:13 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description=' Library for visualizing and understanding predictions of convolutional neural networks.',
+    author='Max Pechyonkin',
+    license='MIT',
+)
