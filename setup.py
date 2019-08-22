ceb9c00f151be4eaf6fb5658fed05e8ae12d07ec
pechyonkin <maxim.pechyonkin@gmail.com>
2019-08-22 18:16:10 -0400

    Change src to sjaandi, update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
-from setuptools import find_packages, setup
+import setuptools
 
-setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
-    description=' Library for visualizing and understanding predictions of convolutional neural networks.',
+with open('README.md') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='sjaandi',
+    version='0.1',
+    scripts=None,
     author='Max Pechyonkin',
+    description='Library for visual similarity search and visualization',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/pechyonkin/sjaandi',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'fastai',
+        'matplotlib',
+        'numpy',
+        'scikit-learn',
+        'rasterfairy @ git+git://github.com/pechyonkin/RasterFairy@python3',
+    ],
     license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
 )
