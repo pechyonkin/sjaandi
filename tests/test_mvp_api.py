e262458f339c934f9163425a5895153b085ee58b
pechyonkin <maxim.pechyonkin@gmail.com>
2019-08-25 18:56:22 -0400

    TESTS: test for images validation
    Validate and raise errors if:
    - corrupt images
    - non-images present in the folder
    - non-images disguised as images are present in the folder

--- tests/test_mvp_api.py
+++ tests/test_mvp_api.py
@@ -1,25 +1,19 @@
+import os
 from typing import Tuple
 import pytest
-from uuid import uuid4
-from tempfile import NamedTemporaryFile
 from tempfile import mkdtemp
+from tempfile import NamedTemporaryFile
+from uuid import uuid4
 
 from sjaandi import VisualSearchEngine
-from sjaandi.usecases.mvp import NonExistentPathError
 from sjaandi.usecases.mvp import EmptyFolderError
+from sjaandi.usecases.mvp import InvalidImageError
 from sjaandi.usecases.mvp import PathIsNotFolderError
-
-
-TEST_DATA_PATH = 'data/raw/test-data'
-NONEXISTENT_PATH = str(uuid4())
-FILE_PATH = NamedTemporaryFile().name
-print(FILE_PATH)
-EMPTY_FOLDER_PATH = mkdtemp()
-PCT_PARTIAL = 0.05  # percentage of full data to use
+from sjaandi.usecases.mvp import NonExistentPathError
 
 
 def save_image(folder: str,
-               resolution: Tuple[int, int] = (300,300),
+               resolution: Tuple[int, int] = (300, 300),
                channels: int = 3,
                extension: str = '.jpg'):
     """
@@ -31,46 +25,124 @@ def save_image(folder: str,
     :param extension:
     :return:
     """
+
     pass
+
+
+def make_folder_with_files(extension: str = '.jpg', n_files=5) -> str:
+    """
+    Make a temp folder with n files of given extension.
+
+    Files are filled with a random string.
+
+    :param extension: file extension, either with dot or not
+    :param n_files: number of files to generate
+    :return: path to the folder with files
+    """
+
+    temp_dir = mkdtemp()
+    for i in range(n_files):
+        file_path = os.path.join(temp_dir, f"some-file-{i}.{extension.strip('.')}")
+        with open(file_path, 'w') as f:
+            f.write(str(uuid4()))
+            f.close()
+    return temp_dir
 
 
 @pytest.fixture()
-def empty_data_path():
-    pass
-
-
-def test_api_throws_non_existent_path_error_with_nonexistent_path():
+def empty_folder_path() -> str:
     """
-    VisualSearchEngine should raise an error when passed a path
-    that doesn't exist.
+    Create an empty folder.
+    :return: path to folder
     """
-    with pytest.raises(NonExistentPathError):
-        engine = VisualSearchEngine(NONEXISTENT_PATH,
-                                    pct_partial=PCT_PARTIAL)
+
+    return mkdtemp()
 
 
-def test_api_throws_non_folder_error_with_file_path():
+@pytest.fixture()
+def temp_file_path() -> str:
     """
-    VisualSearchEngine should raise an error when passed a path
-    that is not a folder.
+    Create a temp file.
+    :return: path to the file
     """
-    temp = NamedTemporaryFile()
-    with pytest.raises(PathIsNotFolderError):
-        engine = VisualSearchEngine(temp.name,
-                                    pct_partial=PCT_PARTIAL)
-    temp.close()
+
+    return NamedTemporaryFile().name
 
 
-def test_api_throws_empty_folder_error_with_empty_folder_path():
+@pytest.fixture()
+def nonexistent_path() -> str:
     """
-    VisualSearchEngine should raise an error when passed a path
-    to a folder that is empty.
+    Create path that doesn't exits.
+    :return: non-existent path
     """
-    with pytest.raises(EmptyFolderError):
-        engine = VisualSearchEngine(EMPTY_FOLDER_PATH,
-                                    pct_partial=PCT_PARTIAL)
+
+    return str(uuid4())
 
 
+@pytest.fixture()
+def non_images_path() -> str:
+    """
+    Make a folder with 5 random PDF files.
+    :return: path to the folder
+    """
+
+    return make_folder_with_files('.pdf')
 
 
+@pytest.fixture()
+def evil_images_path() -> str:
+    """
+    Make a folder with 5 invalid JPG files.
+    :return: path to the folder
+    """
 
+    return make_folder_with_files('.jpg')
+
+
+class TestInvalidDataPath:
+    def test_api_throws_non_existent_path_error_with_nonexistent_path(self, nonexistent_path):
+        """
+        VisualSearchEngine should raise an error when passed a path
+        that doesn't exist.
+        """
+
+        with pytest.raises(NonExistentPathError):
+            engine = VisualSearchEngine(nonexistent_path)
+
+    def test_api_throws_non_folder_error_with_file_path(self):
+        """
+        VisualSearchEngine should raise an error when passed a path
+        that is not a folder.
+        """
+
+        with NamedTemporaryFile() as temp:
+            with pytest.raises(PathIsNotFolderError):
+                engine = VisualSearchEngine(temp.name)
+
+    def test_api_throws_empty_folder_error_with_empty_folder_path(self, empty_folder_path):
+        """
+        VisualSearchEngine should raise an error when passed a path
+        to a folder that is empty.
+        """
+
+        with pytest.raises(EmptyFolderError):
+            engine = VisualSearchEngine(empty_folder_path)
+
+    def test_api_throws_error_for_path_with_non_images(self, non_images_path):
+        """
+        VisualSearchEngine should raise an error when passed a path
+        to a folder with files that are not images.
+        """
+
+        with pytest.raises(InvalidImageError):
+            engine = VisualSearchEngine(non_images_path)
+
+    def test_api_throws_error_for_path_with_evil_images(self, evil_images_path):
+        """
+        VisualSearchEngine should raise an error when passed a path
+        to a folder with files with images extensions but that are
+        not images initially.
+        """
+
+        with pytest.raises(InvalidImageError):
+            engine = VisualSearchEngine(evil_images_path)
